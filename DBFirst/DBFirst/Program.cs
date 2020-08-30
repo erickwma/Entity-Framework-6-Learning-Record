2c3b42d84ce529ecdf91faf7f85cb935e006a989
King Wai Ma <kwma.connect@gmail.com>
2020-08-30 14:53:46 +0800

    Imported stored procedures and wrote a simple demo that iterating out the course title from the datebase

--- DBFirst/DBFirst/Program.cs
+++ DBFirst/DBFirst/Program.cs
@@ -10,6 +10,12 @@ namespace DBFirst
     {
         static void Main(string[] args)
         {
+            var dbContext = new PlutoDbContext();
+            var courses = dbContext.GetCourses();
+            foreach (var c in courses)
+            {
+                Console.WriteLine(c.Title);
+            }
         }
     }
 }
