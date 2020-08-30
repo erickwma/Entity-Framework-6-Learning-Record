a900764e9d51d732eca644d255a7629707d46573
King Wai Ma <kwma.connect@gmail.com>
2020-08-30 17:30:20 +0800

    Tried 2 ways to add enum and change function in the conceptual level without changing in database level

--- DBFirst/DBFirst/Program.cs
+++ DBFirst/DBFirst/Program.cs
@@ -6,16 +6,25 @@ using System.Threading.Tasks;
 
 namespace DBFirst
 {
+    public enum Level : byte
+    {
+        Beginner = 1,
+        Intermediate = 2,
+        Advance = 3
+    }
     class Program
     {
         static void Main(string[] args)
         {
             var dbContext = new PlutoDbContext();
+            dbContext.GetCourses();
             var courses = dbContext.GetCourses();
             foreach (var c in courses)
             {
                 Console.WriteLine(c.Title);
             }
+            var myCourse = new Course();
+            myCourse.Level = CourseLevel.Beginner;
         }
     }
 }
