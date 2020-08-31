f6ab9a4b585c75be2a3ae2adf44f5e5241f2d153
King Wai Ma <kwma.connect@gmail.com>
2020-08-31 12:47:52 +0800

    Demo of migration of modifying an existing class by deleting a existing column

--- CodeFirstWithLegacyDb/CodeFirstWithLegacyDb/Course.cs
+++ CodeFirstWithLegacyDb/CodeFirstWithLegacyDb/Course.cs
@@ -23,8 +23,6 @@ namespace CodeFirstWithLegacyDb
 
         public Category Category { get; set; }
 
-        public DateTime? DatePublished { get; set; }
-
         public int Level { get; set; }
 
         public float FullPrice { get; set; }
