a26b6a9ed1eb7aadc0d2a8003c1b6f1dff67934f
King Wai Ma <kwma.connect@gmail.com>
2020-09-01 15:37:58 +0800

    The very first demo on db queries on Entity Framework 6 using LINQ syntax and extension methods

--- Queries/Queries/Program.cs
+++ Queries/Queries/Program.cs
@@ -1,10 +1,31 @@
-﻿
+﻿using System.Linq;
+
+using System;
+
 namespace Queries
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var context = new PlutoContext();
+
+            // LINQ syntax
+            var query =
+                from c in context.Courses
+                where c.Name.Contains("c#")
+                orderby c.Name
+                select c;
+            foreach (var course in query)
+                Console.WriteLine(course.Name);
+
+            // Extension methods
+            var courses = context.Courses
+                .Where(c => c.Name.Contains("c#"))
+                .OrderBy(c => c.Name);
+
+            foreach (var course in courses)
+                Console.WriteLine(course.Name);
         }
     }
 }
