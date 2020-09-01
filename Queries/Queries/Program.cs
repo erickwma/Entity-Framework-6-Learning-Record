84b0906ab232c678dfc8546a169ae8778753edc4
King Wai Ma <kwma.connect@gmail.com>
2020-09-01 15:59:58 +0800

    Demo of LINQ Restriction, Ordering, Projection, Grouping

--- Queries/Queries/Program.cs
+++ Queries/Queries/Program.cs
@@ -10,22 +10,61 @@ namespace Queries
         {
             var context = new PlutoContext();
 
-            // LINQ syntax
+            //// LINQ syntax
+            //var query =
+            //    from c in context.Courses
+            //    where c.Name.Contains("c#")
+            //    orderby c.Name
+            //    select c;
+            //foreach (var course in query)
+            //    Console.WriteLine(course.Name);
+
+            //// Extension methods
+
+            //    .Where(c => c.Name.Contains("c#"))
+            //    .OrderBy(c => c.Name);
+
+            //foreach (var course in courses)
+            //    Console.WriteLine(course.Name);
+
+            //LINQ Syntax
+
+
+            //Restriction
+            //var query =
+            //    from c in context.Courses
+            //    where c.Level == 1 && c.Author.Id == 1
+            //    select c;
+
+            //Ordering
+            //var query =
+            //    from c in context.Courses
+            //    where c.Author.Id == 1
+            //    orderby c.Level descending, c.Name
+            //    select c;
+
+            //Projection
+            //var query =
+            //    from c in context.Courses
+            //    where c.Author.Id == 1
+            //    orderby c.Level descending, c.Name
+            //    select new { Name = c.Name, Author = c.Author.Name };
+
+            //Grouping
             var query =
-                from c in context.Courses
-                where c.Name.Contains("c#")
-                orderby c.Name
-                select c;
-            foreach (var course in query)
-                Console.WriteLine(course.Name);
+            from c in context.Courses
+            group c by c.Level
+            into g
+            select g;
 
-            // Extension methods
-            var courses = context.Courses
-                .Where(c => c.Name.Contains("c#"))
-                .OrderBy(c => c.Name);
+            foreach (var group in query)
+            {
+                Console.WriteLine("{0} ({1})", group.Key, group.Count());
 
-            foreach (var course in courses)
-                Console.WriteLine(course.Name);
+                //foreach (var course in group)
+                //    Console.WriteLine("\t{0}", course.Name);
+            }
+
         }
     }
 }
